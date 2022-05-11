bb7319d9f8f3c15bd01a693b97602b63b33f6089
Yinan Li <gabriel.li5566@gmail.com>
2022-05-12 01:01:30 +0300

    Learn 60-nested routes

--- src/55-pages/Home.js
+++ src/55-pages/Home.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 
 export default class Home extends Component {
   render() {
-    return <div>Home Component</div>
+    return (
+      <div>
+        <h3>Home Component</h3>
+      </div>
+    )
   }
 }
--- src/55-pages/My.js
+++ src/55-pages/My.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
 
 export default class My extends Component {
   render() {
-    return <div>My Account Component</div>
+    return (
+      <div>
+        My Account Component
+        <ul>
+          <li>
+            <NavLink to="profile">Profile</NavLink>
+          </li>
+          <li>
+            <NavLink to="privacy">Privacy</NavLink>
+          </li>
+        </ul>
+        <Outlet />
+      </div>
+    )
   }
 }
