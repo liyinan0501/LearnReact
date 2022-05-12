475712469c4fb9fad1f26b98c6c240bdc5cc708d
Yinan Li <gabriel.li5566@gmail.com>
2022-05-12 13:54:34 +0300

    subRoute

--- src/55-pages/My.js
+++ src/55-pages/My.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
-export default class My extends Component {
+class My extends Component {
   render() {
     return (
       <div>
         My Account Component
+        <br />
+        <button onClick={this.handleClick}>Login</button>
         <ul>
           <li>
             <NavLink to="profile">Profile</NavLink>
@@ -18,4 +21,14 @@ export default class My extends Component {
       </div>
     )
   }
+  handleClick = () => {
+    console.log(this.props)
+    console.log('Need to login')
+    this.props.navigate('/')
+  }
 }
+export const withNavigation = (Component) => {
+  return (props) => <Component {...props} navigate={useNavigate()} />
+}
+
+export default withNavigation(My)
